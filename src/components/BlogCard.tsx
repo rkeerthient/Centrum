19c7dc332c102d525cab208e8d97110081af9743
rkeerthi <sunnykeerthi@gmail.com>
2023-07-14 01:12:45 +0530

    fixed blogs nav

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -3,11 +3,9 @@ import * as React from "react";
 import { Image } from "@yext/pages/components";
 
 const BlogCard = (props: CardProps<any>): JSX.Element => {
-  console.log(JSON.stringify(props));
-
   const { result } = props;
   const { name } = result;
-  const { primaryPhoto, description, slug } = result.rawData;
+  const { primaryPhoto, description, slug, id } = result.rawData;
 
   return (
     <div className="w-full my-4 border">
@@ -17,7 +15,7 @@ const BlogCard = (props: CardProps<any>): JSX.Element => {
         <div className="space-y-2 px-4 pb-4">
           <div className="flex w-auto gap-4 flex-wrap ">{description}</div>
           <div className="rounded-full bg-[#e3005d] hover:bg-black px-8 py-2  w-fit  font-bold text-white mx-auto">
-            <a href={`/${slug}`}>Read blog</a>
+            <a href={`/blogs/${id}`}>Read blog</a>
           </div>
         </div>
       </div>
